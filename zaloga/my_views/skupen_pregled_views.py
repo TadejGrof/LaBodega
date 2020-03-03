02aad8db4467456998fab96e6e7af85f64b38da9
TadejGrof <tadej.grof.semenic@hotmail.com>
2020-03-03 18:59:23 +0100

    nadgradnja

--- zaloga/my_views/skupen_pregled_views.py
+++ zaloga/my_views/skupen_pregled_views.py
@@ -14,32 +14,28 @@ def skupen_pregled_narocil(request, zaloga):
     zaloga = Zaloga.objects.get(pk = zaloga)
     baze = Baza.objects.filter(status="aktivno", tip="vele_prodaja")
     narocila = baze.values('pk','stranka__ime','stranka__pk')
-    vnosi = Vnos.objects.filter(baza__in = baze).order_by('dimenzija').values('baza__pk','dimenzija__dimenzija','tip','stevilo','pk','baza__stranka__pk')
+    vnosi = Vnos.objects.filter(baza__in = baze).order_by('dimenzija').values('baza__pk','dimenzija__dimenzija','dimenzija__radius','tip','stevilo','pk','baza__stranka__pk')
     razlicne_dimenzije = {}
     for vnos in vnosi:
-        dimenzija_tip = vnos['dimenzija__dimenzija'] + '_' + vnos['tip']
+        dimenzija = vnos['dimenzija__dimenzija']
+        tip = vnos['tip']
+        dimenzija_tip = dimenzija + '_' + tip
+        radius = vnos['dimenzija__radius']
         stevilo = vnos['stevilo']
         baza = vnos['baza__pk']
         vnos = vnos['pk']
         if not dimenzija_tip in razlicne_dimenzije:
-            razlicne_dimenzije[dimenzija_tip] = {baza:{vnos:stevilo}}
-        elif not baza in razlicne_dimenzije[dimenzija_tip]:
-            razlicne_dimenzije[dimenzija_tip][baza] = {vnos:stevilo}
+            razlicne_dimenzije[dimenzija_tip] = {'dimenzija':dimenzija,'radius':radius,'tip':tip,'baze':{baza:{vnos:stevilo}}}
+        elif not baza in razlicne_dimenzije[dimenzija_tip]['baze']:
+            razlicne_dimenzije[dimenzija_tip]['baze'][baza] = {vnos:stevilo}
         else:
-            razlicne_dimenzije[dimenzija_tip][baza][vnos] = stevilo
-    skupno = {}
-    for dimenzija in razlicne_dimenzije:
-        sestevek = 0
-        for baza in razlicne_dimenzije[dimenzija]:
-            for vnos in razlicne_dimenzije[dimenzija][baza]:
-                sestevek += razlicne_dimenzije[dimenzija][baza][vnos]
-        skupno[dimenzija] = sestevek
+            razlicne_dimenzije[dimenzija_tip]['baze'][baza][vnos] = stevilo
     slovar = {
         'narocila':narocila,
         'razlicne_dimenzije':razlicne_dimenzije,
         'na_voljo': zaloga.dimenzija_tip_zaloga,
-        'skupno':skupno
         }
+
     return pokazi_stran(request,'zaloga/skupen_pregled_narocil.html', slovar ) 
 
 

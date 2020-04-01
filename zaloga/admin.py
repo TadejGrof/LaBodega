fef6f015523f9d9a0c004e9c1a60c4ac408552bf
TadejGrof <tadej.grof.semenic@hotmail.com>
2020-04-01 19:41:36 +0000

    Baza

--- zaloga/admin.py
+++ zaloga/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Dimenzija, Sestavina, Zaloga, Vnos,Baza, Stroski_Group
+from .models import Dimenzija, Sestavina, Zaloga, Vnos,Baza, Stroski_Group, Kontejner
 
 admin.site.register(Zaloga)
 admin.site.register(Dimenzija)
@@ -7,3 +7,4 @@ admin.site.register(Sestavina)
 admin.site.register(Baza)
 admin.site.register(Vnos)
 admin.site.register(Stroski_Group)
+admin.site.register(Kontejner)

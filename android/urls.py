aee1287a51d62f953081a27367ad5d25034aa97e
TadejGrof <tadej.grof.semenic@hotmail.com>
2021-04-10 19:24:54 +0200

    dodatek

--- android/urls.py
+++ android/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns = [
     path('dimenzije/',views.dimenzije,name="dimenzije"),
-    path('authenticate/',views.authenticate,name="authenticate")
+    path('authenticate/',views.authenticate,name="authenticate"),
+    path('get_csrf/',views.get_csrf,name="get_csrf")
 ]
--- android/views.py
+++ android/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from zaloga.models import Dimenzija
 from django.db.models import F
 from django.contrib.auth import authenticate as auth
 from django.contrib.auth.models import User
+from django.middleware.csrf import get_token
 
 def authenticate(request):
     json= {"authanticated":False}
@@ -26,3 +27,7 @@ def dimenzije(request):
     .annotate(special=F("special"))\
     .values("id","radij","sirina","visina","special"))
     return JsonResponse(dimenzije,safe=False)
+
+def get_csrf(request):
+    csrf = get_token(request)
+    return HttpResponse(csrf)
